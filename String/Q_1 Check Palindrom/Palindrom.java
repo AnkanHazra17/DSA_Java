5ce033e9f51dda69443658bda02c21346a4437f0
AnkanHazra17 <ankan.hazra1710@gmail.com>
2023-06-30 17:11:25 +0530

    String started

---
+++ String/Q_1 Check Palindrom/Palindrom.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Palindrom{
+
+    static boolean check(String str){
+        str = str.toLowerCase();
+        int start = 0;
+        int end = str.length(); 
+
+        while(start <= end){
+            if(str.charAt(start) != str.charAt(end - 1)){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(check(str));
+    }
+
+}
