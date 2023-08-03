1e57875d4f563d1672be656f3130c46f49a09eb3
AnkanHazra17 <ankan.hazra1710@gmail.com>
2023-08-03 18:42:49 +0530

    Qusetion added

--- Array/Basic_Questions/Strivers DSA/Question_27 Longest Successive Elements/Solution.java
+++ Array/Basic_Questions/Strivers DSA/Question_27 Longest Successive Elements/Solution.java
@@ -1,4 +1,14 @@
 
+// Question: Longest Successive Elements
+// Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+// Example 1:
+
+// Input: nums = [100,4,200,1,3,2]
+// Output: 4
+// Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+
+
 
 import java.util.Arrays;
 
