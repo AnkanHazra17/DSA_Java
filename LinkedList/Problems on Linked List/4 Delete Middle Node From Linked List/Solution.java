90a5e87e12384e17ce74dfefb001066ad07b02be
AnkanHazra17 <ankan.hazra1710@gmail.com>
2023-09-29 17:31:02 +0530

    Solution for Delete middle node from the linked list

---
+++ LinkedList/Problems on Linked List/4 Delete Middle Node From Linked List/Solution.java
@@ -0,0 +1,102 @@
+
+
+// Question: Delete the Middle Node of a Linked List
+//Link: https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/description/
+
+class LinkedListLL {
+    class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public Node head;
+    public Node tail;
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void printList() {
+        Node temp = head; // Store head to the temp
+
+        while (temp != null) { // Traversethe list untill null is occured
+            System.out.print(temp.data + " -> "); // print data
+            temp = temp.next; // move temp to the next node
+        }
+        System.out.println("null");
+    }
+
+    // Function to find the middle node of a singly linked list
+    public Node findMiddle(Node head){
+
+        // Initialize two pointers, 'slow' and 'fast,' both starting at the head of the list
+        Node slow = head;
+        Node fast = head;
+
+        // Traverse the list with 'fast' moving two steps at a time and 'slow' moving one step at a time
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // 'slow' now points to the middle node, so return it
+        return slow;
+    }
+
+    // Function to delete the middle node of a singly linked list
+    public Node deleteMiddle(Node head) {
+
+        // Check if the list is empty or has only one element
+        if(head == null || head.next == null){
+            return null;   // There's no middle node to delete
+        }
+
+        // Find the middle node using the 'findMiddle' function
+        Node midNode = findMiddle(head);
+        // Initialize a temporary pointer 'temp' to traverse the list
+        Node temp = head;
+
+        // Traverse the list until 'temp' reaches the node before the middle node
+        while(temp.next != midNode){
+            temp = temp.next;
+        }
+
+        // Update the 'next' pointer of the previous node to skip the middle node, effectively deleting it
+        temp.next = temp.next.next;
+        // Return the head of the modified list
+        return head;
+    }
+}
+
+public class Solution{
+
+    public static void main(String[] args) {
+        LinkedListLL ll = new LinkedListLL();
+
+        ll.addLast(1);
+        // ll.addLast(3);
+        // ll.addLast(4);
+        // ll.addLast(7);
+        // ll.addLast(1);
+        // ll.addLast(2);
+        // ll.addLast(6);
+
+        ll.printList();
+        ll.deleteMiddle(ll.head);
+        ll.printList();
+
+    }
+}
